49639921042f9cf2adeb84611146b6681607e503
Ethan <roancsu@163.com>
2019-07-18 10:37:36 +0800

    add ubuntu deploy

--- install_k8s_ubuntu_master.sh
+++ install_k8s_ubuntu_master.sh
@@ -2,18 +2,36 @@
 
 set -e
 
-echo "install kubernetes docker on linux ..."
+echo "install kubernetes docker on ubuntu ..."
 
 echo "set aliyun resource ..."
 #set aliyun resource
 
-cat <<EOF > /etc/yum.repos.d/kubernetes.repo
-[kubernetes]
-name=Kubernetes
-baseurl=https://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64/
-enabled=1
-gpgcheck=0
+echo "" > /etc/apt/sources.list
+
+cat <<EOF> /etc/apt/sources.list
+deb http://mirrors.163.com/ubuntu/ xenial main
+deb-src http://mirrors.163.com/ubuntu/ xenial main
+
+deb http://mirrors.163.com/ubuntu/ xenial-updates main
+deb-src http://mirrors.163.com/ubuntu/ xenial-updates main
+
+deb http://mirrors.163.com/ubuntu/ xenial universe
+deb-src http://mirrors.163.com/ubuntu/ xenial universe
+deb http://mirrors.163.com/ubuntu/ xenial-updates universe
+deb-src http://mirrors.163.com/ubuntu/ xenial-updates universe
+
+deb http://mirrors.163.com/ubuntu/ xenial-security main
+deb-src http://mirrors.163.com/ubuntu/ xenial-security main
+deb http://mirrors.163.com/ubuntu/ xenial-security universe
+deb-src http://mirrors.163.com/ubuntu/ xenial-security universe
 EOF
+
+curl -fsSL https://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | apt-key add -
+
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
+
+apt-get update
 
 echo "set machine core ..."
 #set machine core
@@ -22,16 +40,17 @@ echo "set machine core ..."
 # net.bridge.bridge-nf-call-iptables = 1
 # EOF
 
-#sysctl -p
+# sysctl -p
 
 
 #update
-yum -y update
+apt-get -y update
 
 
 echo "install kubelet kubectl kubeadm and docker ..."
 #install Kubelet and Docker
-yum install -y docker kubelet kubeadm kubectl kubernetes-cni
+apt-get install -y docker.io kubelet kubeadm kubectl kubernetes-cni
+
 systemctl enable docker
 systemctl start docker
 systemctl enable kubelet
@@ -40,7 +59,7 @@ systemctl enable kubelet
 echo "init kubernetes master ..."
 #init the master
 #kubernetes version is v1.15.0 
-kubeadm init --image-repository registry.aliyuncs.com/google_containers --kubernetes-version v1.15.0 --pod-network-cidr=10.0.0.0/16
+kubeadm init --image-repository registry.aliyuncs.com/google_containers --kubernetes-version v1.15.0 --pod-network-cidr=10.244.0.0/16
 
 
 echo "set kube config ..."
@@ -76,6 +95,8 @@ echo "close selinux ..."
 #close selinux
 sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
 setenforce 0
+
+swapoff -a
 
 echo "master install kubernetes docker finished ..."
 
---
+++ install_k8s_ubuntu_node.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+set -e
+
+echo "install kubernetes docker on linux ..."
+
+echo "set aliyun resource ..."
+#set aliyun resource
+
+echo "" > /etc/apt/sources.list
+
+cat <<EOF> /etc/apt/sources.list
+deb http://mirrors.163.com/ubuntu/ xenial main
+deb-src http://mirrors.163.com/ubuntu/ xenial main
+
+deb http://mirrors.163.com/ubuntu/ xenial-updates main
+deb-src http://mirrors.163.com/ubuntu/ xenial-updates main
+
+deb http://mirrors.163.com/ubuntu/ xenial universe
+deb-src http://mirrors.163.com/ubuntu/ xenial universe
+deb http://mirrors.163.com/ubuntu/ xenial-updates universe
+deb-src http://mirrors.163.com/ubuntu/ xenial-updates universe
+
+deb http://mirrors.163.com/ubuntu/ xenial-security main
+deb-src http://mirrors.163.com/ubuntu/ xenial-security main
+deb http://mirrors.163.com/ubuntu/ xenial-security universe
+deb-src http://mirrors.163.com/ubuntu/ xenial-security universe
+EOF
+
+curl -fsSL https://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | apt-key add -
+
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
+
+apt-get update
+
+echo "set machine core ..."
+#set machine core
+# cat <<EOF > /etc/sysctl.conf
+# net.bridge.bridge-nf-call-ip6tables = 1
+# net.bridge.bridge-nf-call-iptables = 1
+# EOF
+
+# sysctl -p
+
+
+#update
+apt-get -y update
+
+
+echo "install kubelet kubectl kubeadm and docker ..."
+#install Kubelet and Docker
+apt-get install -y docker.io kubelet kubeadm kubectl kubernetes-cni
+systemctl enable docker
+systemctl start docker
+systemctl enable kubelet
+
+
+echo "pull docker images ..."
+
+docker pull registry.aliyuncs.com/google_containers/kube-proxy:v1.15.0
+docker pull registry.aliyuncs.com/google_containers/coredns:1.3.1
+docker pull registry.aliyuncs.com/google_containers/pause:3.1
+
+
+echo "close selinux ..."
+#close selinux
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
+setenforce 0
+
+echo "node install kubernetes docker finished ..."
+
+
+
+
